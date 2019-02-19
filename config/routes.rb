5c58613ebd0772b89d491a39ffff30c03cbda21b
Mark Gould <m.gould16@gmail.com>
2019-02-19 13:58:42 +0000

    work on showing description and tick box complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all tasks
+  get 'tasks', to: 'tasks#index'
+
+
+
+  # Read the details of one restaurant
+  get 'task/:id', to: 'tasks#show'
+
+
+
 end
